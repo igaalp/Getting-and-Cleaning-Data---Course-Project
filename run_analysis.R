2f5fa8c421e933827235bf20ef5496eb8fa3f4e4
igaalp <igaal.perez@gmail.com>
2016-06-01 16:37:11 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,29 +2,29 @@ library(stringr); library(dplyr)
 #setwd("I:/Data scientist/3 - Getting and Cleaning Data/W5 - Project/UCI HAR Dataset")
 setwd("C:/Users/IP/Documents")
 
-#retrieveing the names of "test" and "train" files for the folder "Inertial Signals",  
-testfiles<-list.files(path="~/test/Inertial Signals")
-trainfiles<-list.files(path="~/train/Inertial Signals")
+#retrieveing the names of "test" and "train" files for the folder "Inertial Signals",  ( optional)
+# testfiles<-list.files(path="~/test/Inertial Signals")
+# trainfiles<-list.files(path="~/train/Inertial Signals")
 
-#Gathering Intnertial Signals (train and test) into one table for each variable  ( optional)
+#Gathering Inertial Signals (train and test) into one table for each variable  
 
-#body_acc_x<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[1])),read.table(paste0("~/train/Inertial Signals/", trainfiles[1])))
+# body_acc_x<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[1])),read.table(paste0("~/train/Inertial Signals/", trainfiles[1])))
 
-#body_acc_y<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[2])), read.table(paste0("~/train/Inertial Signals/", trainfiles[2])))
+# body_acc_y<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[2])), read.table(paste0("~/train/Inertial Signals/", trainfiles[2])))
 
-#body_acc_z<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[3])), read.table(paste0("~/train/Inertial Signals/", trainfiles[3])))
+# body_acc_z<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[3])), read.table(paste0("~/train/Inertial Signals/", trainfiles[3])))
 
-#body_gyro_x<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[4])), read.table(paste0("~/train/Inertial Signals/", trainfiles[4])))
+# body_gyro_x<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[4])), read.table(paste0("~/train/Inertial Signals/", trainfiles[4])))
 
-#body_gyro_y<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[5])), read.table(paste0("~/train/Inertial Signals/", trainfiles[5])))
+# body_gyro_y<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[5])), read.table(paste0("~/train/Inertial Signals/", trainfiles[5])))
 
-#body_gyro_z<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[6])), read.table(paste0("~/train/Inertial Signals/", trainfiles[6])))
+# body_gyro_z<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[6])), read.table(paste0("~/train/Inertial Signals/", trainfiles[6])))
 
-#total_acc_x<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[7])), read.table(paste0("~/train/Inertial Signals/", trainfiles[7])))
+# total_acc_x<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[7])), read.table(paste0("~/train/Inertial Signals/", trainfiles[7])))
 
-#total_acc_y<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[8])), read.table(paste0("~/train/Inertial Signals/", trainfiles[8])))
+# total_acc_y<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[8])), read.table(paste0("~/train/Inertial Signals/", trainfiles[8])))
 
-#total_acc_z<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[9])), read.table(paste0("~/train/Inertial Signals/", trainfiles[9])))
+# total_acc_z<-rbind(read.table(paste0("~/test/Inertial Signals/", testfiles[9])), read.table(paste0("~/train/Inertial Signals/", trainfiles[9])))
 
 
 
